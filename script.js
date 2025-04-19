ff465c8533e75f7dc7a954a19fb581e7d3cc2e49
danielkuciej <biuro.symmetry@gmail.com>
2025-04-19 22:11:44 +0200

    Added close mobile-nav after click function

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 "use script";
 
+/////////////////////////////
 // Current year
 const yearEl = document.querySelector(".year");
 const newYear = new Date().getFullYear();
 yearEl.textContent = newYear;
 
+/////////////////////////////
 //Mobile navigation
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+/////////////////////////////
 //Sticky mavigation
 const sectionHeroEl = document.querySelector(".section-hero");
 
@@ -26,6 +54,10 @@ const obs = new IntersectionObserver(
 
     if (ent.isIntersecting === true) {
       document.body.classList.remove("sticky");
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.remove("nav-open");
     }
   },
   {
